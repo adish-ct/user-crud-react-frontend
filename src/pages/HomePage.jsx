7800613b3800e40eb9e1feec1797978cdced6bf8
adish-ct <adishathu525@gmail.com>
2023-10-26 18:31:49 +0530

    backend_base_url created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import axios from 'axios'
+import { BACKEND_BASE_URL } from '../components/Api/Api';
+
 
 function HomePage() {
+
+    const [data, setData] = useState('');
+
+    const fetchTempData = async () => {
+
+        const res = await axios.get(`${BACKEND_BASE_URL}/api/users/data/`)
+        console.log(res.data);
+        setData(res.data)
+    }
+    useEffect(() => {
+        fetchTempData();
+    }, [])
+
     return (
         <div>
             <div className="">
                 <Navbar />
+                {typeof data === 'object' ? (
+                    <p>{data.data}</p>
+                ) : (
+                    <p>{data}</p>
+                )}
             </div>
         </div>
     )
